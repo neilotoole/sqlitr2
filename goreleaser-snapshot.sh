5d810c1bcb7a5c3821ca11002b43b521ac457def
Neil O'Toole <neilotoole@apache.org>
2022-11-01 22:43:09 -0600

    fiddling with gh workflow

--- goreleaser-snapshot.sh
+++ goreleaser-snapshot.sh
@@ -1,9 +1,4 @@
 #!/usr/bin/env bash
 
-#docker run --rm --privileged \
-#-v $(pwd):/go/src/github.com/neilotoole/sqlitr2 \
-#-v /var/run/docker.sock:/var/run/docker.sock \
-#-w /go/src/github.com/neilotoole/sqlitr2 \
-#neilotoole/xcgo:latest goreleaser --snapshot --rm-dist
 
-goreleaser --snapshot --rm-dist -f .goreleaser-darwin.yml
+goreleaser release --snapshot --rm-dist -f .goreleaser-macos.yml
